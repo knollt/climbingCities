05abc4b4284a110797d054959973776bf20a0a26
Tim Knoll <timothy.knoll@gmail.com>
2019-02-05 22:59:12 +0000

    added scraper file.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require 'open-uri'
 
 require_relative "..lib/climbingCities/cities"
 require_relative "..lib/climbingCities/cli"
+require_relative "..lib/climbingCities/scraper"
 require_relative "..lib/climbingCities/version"
