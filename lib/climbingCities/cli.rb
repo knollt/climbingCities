549aae36c6b2273684630ef819376a4c92808ee1
Tim Knoll <timothy.knoll@gmail.com>
2019-02-05 23:03:33 +0000

    modify cli.rb with draft cli commands

--- lib/climbingCities/cli.rb
+++ lib/climbingCities/cli.rb
@@ -2,7 +2,28 @@ class climbingCities::CLI
   
   # start a new CLI session 
   def call 
+    # call scrape
+    list_cities
+    menu
+  end   
+  
+  
+  #list climbing cities 
+  def list_cities
+    puts " "
+    puts "Welcome climbing enthusiast."
+    puts "Where would you like to explore today?"
+    puts " "
+    # load scrape
+    # produce list of cities from scrape
+  end 
+  
+  def menu
     
   end   
   
+  
+  def goodbye
+    
+  end   
 end  
