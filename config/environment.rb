b9bdb2be1b2cbc21c7cb6ef3e7b6ff0de022c205
Tim Knoll <timothy.knoll@gmail.com>
2019-02-05 04:29:03 +0000

    created, setup config>environment file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative "..lib/climbingCities/cities"
+require_relative "..lib/climbingCities/cli"
+require_relative "..lib/climbingCities/version"
