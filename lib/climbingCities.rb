53cbe4a29de9120cf840103ce1fa490f773fbbeb
Tim Knoll <timothy.knoll@gmail.com>
2019-02-06 03:36:48 +0000

    gemspec error fixed update

--- lib/climbingCities.rb
+++ lib/climbingCities.rb
@@ -1,6 +1,11 @@
 # This was environment file and environment file was created/setup in more convenient file under config.
 
 
-require_relative "climbing_cities/version.rb"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+require_relative "climbing_cities/version.rb"
 require_relative "climbing_cities/cli.rb"
+require_relative "climbing_cities/cities"
+require_relative "climbing_cities/scraper"
