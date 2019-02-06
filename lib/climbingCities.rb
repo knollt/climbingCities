c5ba07011c2fc867c64ae870aea0a26929b027e8
Tim Knoll <timothy.knoll@gmail.com>
2019-02-06 00:45:22 +0000

    gemspec setup error progress 2

--- lib/climbingCities.rb
+++ lib/climbingCities.rb
@@ -1,9 +1,6 @@
 # This was environment file and environment file was created/setup in more convenient file under config.
 
 
-# require "climbingCities/version"
+require "climbingCities/version"
 
-# module ClimbingCities
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
+require_relative "..lib/climbingCities/cli"
