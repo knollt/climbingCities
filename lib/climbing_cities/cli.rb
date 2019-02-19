b99bd57c0fa06652d8d85cca3a162b49b41a9248
Tim Knoll <timothy.knoll@gmail.com>
2019-02-19 16:09:20 +0000

    desc input error

--- lib/climbing_cities/cli.rb
+++ lib/climbing_cities/cli.rb
@@ -23,7 +23,7 @@ class ClimbingCities::CLI
     input = nil
     while input != "exit"
       puts " "
-      puts "Would you like to learn more about the city?  If yes, please type yes and hit enter.  If you would like to see to the list of cites again, please type list.  Otherwise, type exit."
+      puts "Would you like to learn more about the city?  \nIf yes, please type yes and hit enter.  \nIf you would like to see to the list of cites again, please type list.  \nOtherwise, type exit."
       puts " "
       input = gets.strip.downcase  #get user input
           
@@ -40,6 +40,24 @@ class ClimbingCities::CLI
     end    
   end   
   
+  # takes city choice and fetches city description
+  def choose_city
+    puts " "
+    puts "Please enter the number of the city you would like to learn more about 1-17 and hit enter."
+    puts " "
+    
+    input = gets.strip.to_i 
+    if input.between?(1,17)
+      category = input-1
+      display_city_desc (category)
+      submenu
+    else
+      puts "Invalid number."
+      choose_city
+    end 
+  end 
+ 
+ 
   # print continue or exit message
   def submenu
     inputted = nil
@@ -47,11 +65,11 @@ class ClimbingCities::CLI
     puts "Would you like to see another city? Enter Y or N."
     puts " "
     inputted = gets.strip.downcase
-    if inputted == "y" || inputted == "Y"
-      menu
-    elsif inputted == "n" || inputted == "N" # ISSUE prints "please enter # of city" still need to input = exit to exit
-      # goodbye
-      puts "Thanks, please type exit."
+    if inputted == "y"
+      choose_city
+    elsif inputted == "n" # ISSUE prints "please enter # of city" still need to input = exit to exit
+      goodbye
+      # puts "Thanks, please type exit."
     else 
       puts "I don't understand your answer."
       submenu
@@ -65,20 +83,6 @@ class ClimbingCities::CLI
     puts " "
   end   
   
-  def choose_city
-    puts " "
-    puts "Please enter the number of the city you would like to learn more about 1-17 and hit enter."
-    puts " "
-    
-    input = gets.strip.to_i 
-    if input.between?(1,17)
-      category = input-1
-      display_city_desc (category)
-      submenu
-    end 
-  end 
- 
-  
   def list_cities
     ClimbingCities::Cities.all.each.with_index(1) do |city, i|
       print "#{i}. #{city.name}"
@@ -87,7 +91,7 @@ class ClimbingCities::CLI
   end 
   
   def display_city_desc (category)
-    ClimbingCities::Scraper.scrape_city_desc
+    ClimbingCities::Scraper.scrape_city_desc(category)
   end   
   
 end  
